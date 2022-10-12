dc070f58c3c9fa596fb7f1feb01c3e41a3c8d7c0
Robby Lewis <Robbysim37@gmail.com>
2022-10-12 17:10:22 -0400

    finished Sprint_2-Module_3

--- index.js
+++ index.js
@@ -15,8 +15,22 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name,age) {
+  this.name = name
+  this.age = age
+  this.stomach = []
+}
+Person.prototype.eat = function(foodToEat) {
+  if(this.stomach.length >= 10){
+    return
+  }
+  this.stomach.push(foodToEat)
+}
+Person.prototype.poop = function() {
+  this.stomach = []
+}
+Person.prototype.toString = function() {
+  return `${this.name}, ${this.age}`
 }
 
 
@@ -36,9 +50,23 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model,milesPerGallon) {
+  this.model = model
+  this.milesPerGallon = milesPerGallon
+  this.tank = 0
+  this.odometer = 0
 }
+  Car.prototype.fill = function(gallons) {
+    this.tank += gallons
+  }
+  Car.prototype.drive = function(distanceInMiles) {
+    this.odometer += distanceInMiles
+    this.tank -= (distanceInMiles/this.milesPerGallon)
+    if(distanceInMiles > (this.milesPerGallon*this.tank)){
+      return `I ran out of fuel at ${this.odometer + (this.milesPerGallon*this.tank)} miles!`
+    }
+  }
+
 
 
 /*
@@ -49,18 +77,24 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name,age,favoriteToy) {
+  this.name = name
+  this.age = age
+  this.favoriteToy = favoriteToy
+}
+Baby.prototype = Object.create(Person.prototype)
+Baby.prototype.play = function() {
+  return `PlayingWith${this.favoriteToy}`
 }
 
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window Binding - the ENTIRE Javascript program, global level, never use
+  2. Implicit Binding - calls on a SPECIFIC instance of an object
+  3. New Binding - used to create a new object based on an object passed into constructor
+  4. Explicit binding - called to override Implicit binding, using .call or .apply
 */
 
 ///////// END OF CHALLENGE /////////
